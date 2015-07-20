11218e8f9c86a77e118787d039e341f01cec2770
Daniel Moennich <daniel.moennich@gmail.com>
2015-07-20 19:54:10 +0200

    Added handling of POST data.

--- router.js
+++ router.js
@@ -1,15 +1,27 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer");
+var querystring = require("querystring");
+
+var commonHeaders = {"Content-Type": "text/html"};
 
 
 var home = function(request, response){
 
 	if(request.url === "/"){
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		renderer.view(response, "header");
-		renderer.view(response, "search");
-		renderer.view(response, "footer");
-		response.end();
+		if(request.method.toUpperCase() === "GET"){
+			response.writeHead(200, commonHeaders);
+			renderer.view(response, "header");
+			renderer.view(response, "search");
+			renderer.view(response, "footer");
+			response.end();
+		} else {
+			request.on("data", function(postDataBuffer){
+				var postData = querystring.parse(postDataBuffer.toString());
+				response.writeHead(303, {location: "/" + postData.userName});
+				response.end();
+			});
+		}
+
 	}
 
 };
@@ -20,7 +32,7 @@ var user = function(request, response){
 	var userName = request.url.replace("/", "");
 	if(userName.length > 0){
 
-		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.writeHead(200, commonHeaders);
 		renderer.view(response, "header");
 
 		var studentProfile = new Profile(userName);
@@ -38,6 +50,7 @@ var user = function(request, response){
 
 		studentProfile.on("error", function(error){
 			renderer.view(response, "error", {errorMessage: error.message});
+			renderer.view(response, "search");
 			renderer.view(response, "footer");
 			response.end();
 		});
